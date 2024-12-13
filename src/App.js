8a3e365db056ff886390ef4a29ee5766d2ff7608
AhmetCelil <acyalman@gmail.com>
2024-12-13 17:14:31 +0300

    rowlar ayarlandı

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
+import { Container, Row, Col } from "reactstrap";  // Bootstrap Container ve Row, Col bileşenlerini içe aktarıyoruz
 import KullaniciListesi from "./components/KullaniciListesi";
-import KullaniciIzinListesi from "./components/KullaniciIzinListesi";  // Bileşeni içe aktardık
+import KullaniciIzinListesi from "./components/KullaniciIzinListesi";
+import 'bootstrap/dist/css/bootstrap.min.css';  // Bootstrap CSS
 
 function App() {
   return (
-    <div className="App">
-      <KullaniciListesi />
-      <KullaniciIzinListesi />
-
-    </div>
+    <Container className="App">
+      {/* İlk Row, birinci sütun solda, ikinci sütun sağda */}
+      <Row>
+        {/* Kullanıcı Listesi Solda */}
+        <Col sm="6">
+          <KullaniciListesi />
+        </Col>
+        
+        {/* Kullanıcı İzin Listesi Sağda */}
+        <Col sm="6">
+          <KullaniciIzinListesi />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
